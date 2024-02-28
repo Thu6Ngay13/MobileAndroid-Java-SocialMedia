a3b994bfcbdee641a07fc687c8c8ed722d12c2c6
CAO816 <caothithuthuy00000@gmail.com>
2024-02-28 09:55:49 +0700

    Create register page, find your account page
    add logo_541x541, add color green_normal

--- app/src/main/java/HCMUTE/SocialMedia/Activities/RegisterActivity.java
+++ app/src/main/java/HCMUTE/SocialMedia/Activities/RegisterActivity.java
@@ -2,15 +2,54 @@ package HCMUTE.SocialMedia.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import HCMUTE.SocialMedia.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText etBirthday;
+    private Calendar calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etBirthday = findViewById(R.id.etBirthday);
+        calendar = Calendar.getInstance();
+    }
+    public void showDatePickerDialog(View view) {
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, monthOfYear);
+                        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                        updateEditTextBirthday();
+                    }
+                }, year, month, day);
+
+        datePickerDialog.show();
+    }
+
+    private void updateEditTextBirthday() {
+        String dateFormat = "dd/MM/yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.getDefault());
+
+        etBirthday.setText(simpleDateFormat.format(calendar.getTime()));
     }
 }
