9ef98e7d054e9b044a5a72f07418820561db97f0
CAO816 <caothithuthuy00000@gmail.com>
2024-02-29 10:05:52 +0700

    Create FindAccount page, đổi màu nền và icon của app

---
+++ app/src/main/java/HCMUTE/SocialMedia/Activities/FindAccountActivity.java
@@ -0,0 +1,16 @@
+package HCMUTE.SocialMedia.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import HCMUTE.SocialMedia.R;
+
+public class FindAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_account);
+    }
+}
